a6f5ec23d2ebd8bdb959aa693903bd911ad3b000
Alec Gall <22alec22@gmail.com>
2021-06-11 18:40:33 -0400

    PollAnswer and PollCreate components

--- src/Components/Poll/Poll.js
+++ src/Components/Poll/Poll.js
@@ -1,37 +1,11 @@
+import PollAnswer from "./PollAnswer";
+import PollCreate from "./PollCreate";
+
 const Poll = () => {
   return (
     <div className="card-white col-span-2 w-1/2 text-center">
-      <h2 className="mb-4 text-center">This is a test</h2>
-      <p className="mb-4 text-center">
-        Hello, this is some text to fill up this space. Blah blah blah blah.
-        Something cool, something neat, something rad.
-      </p>
-      <form>
-        <div className="mb-4">
-          <div className="flex items-center">
-            <input type="radio" name="Poll" value="" className="mx-4"></input>
-            <p>This one?</p>
-          </div>
-          <div className="flex items-center">
-            <input type="radio" name="Poll" value="" className="mx-4"></input>
-            <p>Maybe this one.</p>
-          </div>
-          <div className="flex items-center">
-            <input type="radio" name="Poll" value="" className="mx-4"></input>
-            <p>Or... Maybe this one?</p>
-          </div>
-          <div className="flex items-center">
-            <input type="radio" name="Poll" value="" className="mx-4"></input>
-            <p>Hmmm... How about this?</p>
-          </div>
-        </div>
-        <button type="submit" className="btn-primary mx-2">
-          Vote
-        </button>
-        <button className="btn-alt-onwhite mx-2">
-          Results
-        </button>
-      </form>
+      {/* <PollAnswer /> */}
+      <PollCreate />
     </div>
   );
 };
