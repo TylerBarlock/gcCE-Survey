611fe9712111659617bad5746e4d023d56370421
Alec Gall <22alec22@gmail.com>
2021-06-13 16:11:04 -0400

    Buttons to open create and answer forms

--- src/App.js
+++ src/App.js
@@ -4,17 +4,7 @@ import Poll from "./Components/Poll/Poll";
 
 function App() {
   return (
-    <div className="bg-primary-50 h-full grid grid-cols-1 md:grid-cols-2 gap-4 m-4 w-auto justify-items-center">
-      <div className="card-white grid-cols-1 text-center">
-        <h1 className="mb-4">GrapeCity Poll System</h1>
-        <p className="mb-4">This is some test text. This is some test text. This is some test text. This is some test text. This is some test text. This is some test text. This is some test text.</p>
-        <button className="btn-primary">Click Me</button>
-      </div> 
-      <div className="card-alt grid-cols-1 text-center">
-        <h2 className="mb-4">GrapeCity Poll System</h2>
-        <p className="mb-4">This is some test text. This is some test text. This is some test text. This is some test text. This is some test text. This is some test text. This is some test text.</p>
-        <button className="btn-alt">Click Me</button>
-      </div>
+    <div>
       <Poll />
     </div>
   );
--- src/Components/Poll/Poll.js
+++ src/Components/Poll/Poll.js
@@ -1,16 +1,59 @@
+import React, { useState } from "react";
 import PollAnswer from "./PollAnswer";
 import PollCreate from "./PollCreate";
 
 const Poll = () => {
+  const saveNewPollHandler = () => {};
 
-const saveNewPollHandler = () => {
-  
-}
+  const [createStatus, setCreateStatus] = useState(false);
+  const [answerStatus, setAnswerStatus] = useState(false);
 
-    return (
-    <div className="card-white col-span-2 w-1/2 text-center">
-      {/* <PollAnswer /> */}
-      <PollCreate onSaveNewPoll={saveNewPollHandler}/>
+  const createClickHandler = () => {
+    setCreateStatus(true);
+    setAnswerStatus(false);
+
+  };
+
+  const answerClickHandler = () => {
+    setCreateStatus(false);
+    setAnswerStatus(true);
+
+  };
+
+  return (
+    <div className="bg-primary-50 h-full grid grid-cols-1 md:grid-cols-2 gap-4 m-4 w-auto justify-items-center">
+      <div className="card-white grid-cols-1 text-center">
+        <h1 className="mb-4">Create a Poll</h1>
+        <p className="mb-4">
+          Click the button below to create your own poll! Click the button below to create your own poll! Click the button below to create your own poll! Click the button below to create your own poll!
+        </p>
+        <button className="btn-primary" onClick={createClickHandler}>
+          Create a Poll
+        </button>
+      </div>
+      <div className="card-alt grid-cols-1 text-center">
+        <h2 className="mb-4">Answer a Poll</h2>
+        <p className="mb-4">
+        Click the button below to respond to a poll! Click the button below to respond to a poll! Click the button below to respond to a poll! Click the button below to respond to a poll!
+        </p>
+        <button className="btn-alt" onClick={answerClickHandler}>
+          Answer a Poll
+        </button>
+      </div>
+      {createStatus && (
+        <div className="card-white col-span-2 w-1/2 text-center">
+          <PollCreate onSaveNewPoll={saveNewPollHandler} />
+        </div>
+      )}
+      {answerStatus && (
+        <div className="card-white col-span-2 w-1/2 text-center">
+          <PollAnswer />
+        </div>
+      )}
+      {/* <div className="card-white col-span-2 w-1/2 text-center">
+        <PollAnswer />
+        <PollCreate onSaveNewPoll={saveNewPollHandler} />
+      </div> */}
     </div>
   );
 };
